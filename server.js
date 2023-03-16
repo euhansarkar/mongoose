c9a2a1873a4d840203d0a86a8e7a3b95ad6c4843
euhansarkar <euhansarkar@gmail.com>
2023-03-17 03:31:28 +0600

    step-1 ----structure

---
+++ server.js
@@ -0,0 +1,29 @@
+const mongoose = require(`mongoose`);
+const dotenv = require(`dotenv`);
+const colors = require(`colors`);
+dotenv.config();
+
+const app = require(`./index`);
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`database connection successful`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
+
+
+// server 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`.yellow.bold)
+})
